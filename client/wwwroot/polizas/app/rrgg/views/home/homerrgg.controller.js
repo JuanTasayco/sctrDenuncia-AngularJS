89512d791b8393016b423eec9ab4e13239f7aa69
Denys Arqueros <denysarqueros@multiplica.com>
2023-09-07 17:15:09 -0500

    Admin-rrgg

--- client/wwwroot/polizas/app/rrgg/views/home/homerrgg.controller.js
+++ client/wwwroot/polizas/app/rrgg/views/home/homerrgg.controller.js
@@ -12,9 +12,9 @@ define([
   function HomeRrggController($state, $scope, isAdmin,riesgosGeneralesFactory,oimPrincipal) {
     (function load_HomeRrggController() {
       var vRole = oimPrincipal.get_role();
-      $scope.clonePermission = isAdmin || _.contains(["ADMINRRGG"], vRole);
+      $scope.clonePermission = isAdmin || _.contains(["ADMIN-RRGG"], vRole);
     })();
-
+    
     $scope.IrACotizar = function () {
       riesgosGeneralesFactory.initCotizacion();
       $state.go(constantsRiesgosGenerales.ROUTES.COTIZACION_STEPS, { step: 1 }, { reload: true, inherit: false });
