2e28c78da411f7697beefa52cf1e6a5318dc0635
Denys Arqueros <denysarqueros@multiplica.com>
2023-10-12 13:24:41 -0500

    control de cambios

--- client/wwwroot/gcw/app/components/cobranzas/liquidacion-soat/liquidacion-soat-historial/liquidacion-soat-historial.component.js
+++ client/wwwroot/gcw/app/components/cobranzas/liquidacion-soat/liquidacion-soat-historial/liquidacion-soat-historial.component.js
@@ -51,6 +51,7 @@ define([
                 if(response.data.code == 1){
 
                   var params = {
+                    userCode: vm.dataTicket.userCode,
                     agentId: vm.rol.agenteID,
                     managerId: vm.rol.gestorID,
                     RowByPage: "10",
