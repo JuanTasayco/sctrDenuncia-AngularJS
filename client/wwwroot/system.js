be84d2c6ab91ec8f34af8a81aa941cdc5b4fd67f
Enrique_Nunez <caquesquen@stefanini.com>
2023-10-20 17:07:03 -0500

    agregar el módulo de inclusion masvia

--- client/wwwroot/system.js
+++ client/wwwroot/system.js
@@ -80,7 +80,7 @@
         menuName: "Gestión de Requerimientos",
         isComponsesMenu: true,
         icon: "ico-mapfre_172_consultaGestion"
-      },	  
+      },
       gcw: {
         code: "GCW",
         shortName: "CONSULTAS DE GESTION",
@@ -450,6 +450,18 @@
         rootApi: '',
         isComponsesMenu: true,
         icon: 'ico-mapfre_93_crearmodificar'
+      },
+      inclusionMasiva:{
+        code: "OPTI",
+        shortName: 'Inclusión Masiva de Salud',
+        packageName: 'inclusionmasivaPackage',
+        path: "/inclusionmasiva/package",
+        appPath: "/inclusionmasiva/app",
+        location: '/inclusionmasiva/#',
+        menuName: 'Inclusión Masiva de Salud',
+        rootApi: '',
+        isComponsesMenu: true,
+        icon: 'ico-mapfre_343_sctrdenuncia'
       },
     },
     lib: {
