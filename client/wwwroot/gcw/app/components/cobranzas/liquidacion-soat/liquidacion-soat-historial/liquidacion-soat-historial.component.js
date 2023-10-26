9a4debb90145e304b75f70225ef3f0288a7928f0
Denys Arqueros <denysarqueros@multiplica.com>
2023-10-26 12:21:43 -0500

    user code

--- client/wwwroot/gcw/app/components/cobranzas/liquidacion-soat/liquidacion-soat-historial/liquidacion-soat-historial.component.js
+++ client/wwwroot/gcw/app/components/cobranzas/liquidacion-soat/liquidacion-soat-historial/liquidacion-soat-historial.component.js
@@ -159,12 +159,13 @@ define([
 
     $scope.downloadImpresion = function(item,type){
       vm.rol = gcwFactory.obtenerAgente(vm.dataTicket);
+      debugger;
       vm.exportURLDetalle = $sce.trustAsResourceUrl(constants.system.api.endpoints.gcw+ 'api/collection/soat/download?extensionFile='+type);
         vm.downloadFile2 = {
           preSettlement: item.preSettlement,
           agentId: vm.rol.agenteID,
           managerId: vm.rol.gestorID,
-          userCode: vm.dataTicket.security
+          userCode: vm.dataTicket.userCode
         };
         $timeout(function() {
           document.getElementById('frmExportDetalle').submit();
